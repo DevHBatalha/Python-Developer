6c3f783bc5704bfb05126255e040561653bfa600
Hélio Batalha <h.d.t.batalha@gmail.com>
2026-01-22 17:46:22 +0000

    Programa que calcula o dobro, triplo e a raiz quadrada do número,

---
+++ operadores-aritmeticos/operadores-aritmeticos/dobro-triplo-raiz-quadrada.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 22 16:42:04 2026
+
+@author: heliobatalha
+"""
+
+num = int(input('Escreva um número Interiro: '))
+print('O Dobro de {}  é {}  e o seu Triplo é {} a sua raíz quadrada é {:.2f}' .format(num, (num*2), (num*3), (num ** (1/2))))
---
+++ operadores-aritmeticos/operadores-aritmeticos/media-artmetica.py
@@ -0,0 +1,1 @@
+nota1 = float(input("Escreva a primeira nota: "))nota2 = float(input("Escreva a segunda nota: "))media = (nota1 + nota2)/2print('A primeira nota é: {}, a segunda nota é: {} e a sua média ponderada é {:.1f}' .format(nota1, nota2, media))
